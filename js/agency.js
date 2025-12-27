6f730cacb7492eb6e48c59c9555307d38d274cd5
LenaAlban <lenaetalban@outlook.fr>
2025-12-28 00:05:39 +0100

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -17,8 +17,10 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+  target: '.navbar-fixed-top',
+  offset: $('.navbar').outerHeight() + 5
+});
+
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
